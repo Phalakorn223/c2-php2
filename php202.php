05ef7988d3d15080f6d1a57c13f10232124d1434
Phalakorn223 <phalakorn061@gmail.com>
2021-11-12 11:30:54 +0700

    initial commit

--- php202.php
+++ php202.php
@@ -1,3 +1,22 @@
-<?php
-    echo "My Website";
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf8">
+        <meta neme="viewport" content="device=width-device, inital-scale=1.0">
+        <title>PHP: Hypertext Preprocessor.</title>
+    </head>
+    <body>
+        <h1>Welcome to php</h1>
+        <hr />
+
+        <h2>
+            <?php 
+                echo "Hello World!"; 
+                echo "<br />";
+                echo "My Name is Mar Suckerberg";
+                echo "<br />";
+                
+            ?>
+        </h2>
+    </body>
+</html>
